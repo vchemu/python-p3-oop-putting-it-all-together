f46dd4a985df746be86bb5be716b5de53691d88c
vchemu <valerine.chemutai@student.moringaschool.com>
2023-10-10 05:45:17 +0300

    oop

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,18 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
-    
-        
+    def __init__(self, title="And Then There Were None", page_count = 272):
+        self.title = title
+        self.page_count = page_count
+
+    def get_page(self):
+        return self._page_count
+    def set_page(self, page_count):
+        if isinstance(page_count, int):
+            self._page_count = page_count
+        else:
+            print("page_count must be an integer")
+    page_count = property(get_page, set_page)
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
